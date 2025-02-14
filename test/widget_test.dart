e63688db7bc6ea136a66fcb48b53686311042eac
Aman Singh <amansingh@QCLAP2205P1039.local>
2025-02-14 16:55:38 +0530

    should return the number itself for a single number

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,6 +13,11 @@ void main() {
     test('should return 0 for empty string', () {
       expect(add(""), 0);
     });
+
+    test('should return the number itself for a single number', () {
+      expect(add("1"), 1);
+      expect(add("2"), 2);
+    });
   });
 }
 
